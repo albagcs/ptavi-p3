348805287977d3a034fccd87ea3c6d717a5719c9
alba garcia de la camacha selgas <albagc@iota03.aulas.gsyc.es>
2014-10-10 19:58:38 +0200

    Ejercicio 4/p3 con pep8 pasado

---
+++ karaoke.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+from smallsmilhandler import smallSMILHandler
+import sys
+
+
+def Impresion_Ordenada(lista):
+    for Diccionario in lista:
+        print Diccionario['etiqueta'], ('\t'),
+        for Clave in Diccionario:
+            if Clave != 'etiqueta' and Diccionario[Clave] != "":
+                print Clave, "= ", Diccionario[Clave], ('\t'),
+        print
+
+
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    cHandler = smallSMILHandler()
+    parser.setContentHandler(cHandler)
+    try:
+        parser.parse(open(sys.argv[1]))
+    except:
+        sys.exit("Usage: python karaoke.py file.smil.")
+    lista = cHandler.get_tags()
+    Impresion_Ordenada(lista)
